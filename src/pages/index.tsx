415e46c80f9473f8f7a00256f3630490e6f2fa3a
Louise Nielsen <loui59f1@stud.kea.dk>
2023-01-27 15:10:58 +0100

    Final viewport try

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,7 @@ export default function Home() {
       <Head>
         <title>Gesicht</title>
         < meta name="description" content="Cosmetic clinic in Copenhagen" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <link rel="icon" href="/favicon.png" />
