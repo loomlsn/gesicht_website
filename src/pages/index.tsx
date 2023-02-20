8942bd676d3c108c048639dcc2a687ac05cdc1f1
Louise Nielsen <loui59f1@stud.kea.dk>
2023-02-20 11:19:45 +0100

    Removed unsused function

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -64,7 +64,6 @@ export default function Home() {
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-        <meta name="theme-color" content={getThemeColor()} />
         <link rel="icon" href="/favicon.png" />
       </Head>
 
