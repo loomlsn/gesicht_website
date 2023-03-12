93ca8684994cd8ec4917b86481f7bc984b1a8111
Louise Nielsen <loui59f1@stud.kea.dk>
2023-03-12 13:27:08 +0100

    So0mething

--- src/components/HomePage/IntroSection/IntroSection.tsx
+++ src/components/HomePage/IntroSection/IntroSection.tsx
@@ -27,15 +27,9 @@ export default function IntroSection({ title, text, colorTheme, img, link, booki
                                 {data.booking.intro.text.map((item, i) => (
                                     <p key={`booking-text-${i}`}>{item.text}</p>
                                 ))}
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={styles.arrowDown}>
+                                {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={styles.arrowDown}>
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 17.25L12 21m0 0l-3.75-3.75M12 21V3" />
-                                </svg>
-                                {/* <button onClick={() => setBookingOpen(true)}>
-                                    {data.booking.intro.cta}
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
-                                    </svg>
-                                </button> */}
+                                </svg> */}
                             </>
                         </div>
                     ) : (
